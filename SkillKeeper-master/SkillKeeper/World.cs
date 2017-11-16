73323a29cf06216f9bddd86bdc63069a7e457c8c
dbarnes92 <dbarnes@src-solutions.com>
2017-11-16 15:05:00 -0500

    Class-ified the worlds
    Created a world class that only tracks filename and name at the moment. Blocks saving if the world name under Settings is empty and stores a WorldName xml element.

---
+++ SkillKeeper-master/SkillKeeper/World.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillKeeper
+{
+    class World
+    {
+        private String filename = "", name = "";
+
+        public String Filename
+        {
+            get { return filename; }
+            set { filename = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
