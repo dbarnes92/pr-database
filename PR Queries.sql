7a984ccd7c81503719ca71fe1aac5341c3a2ca19
Ofomezie Emelle <oie3@scarletmail.rutgers.edu>
2017-11-26 09:37:58 -0500

    Add files via upload
    Sample SQL Scripts (random data) for Player, Tournament, Region, and Set_Data tables, also Triggers uploaded to test current state of the trigger. Currently only one trigger is active that activates whenever a row is inserted into the set_data table, updating the 2 players respectively.

--- PR Queries.sql
+++ PR Queries.sql
@@ -1,39 +1,39 @@
 /*List of Players by Most set wins*/
 
-SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
+SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, elo, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
 FROM player
 Order By wins desc;
 
 /*List of Players by Most set wins (top 10)*/
 
-SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
+SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, elo, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
 FROM player
 Order By wins desc
 LIMIT 10;
 
 /*List of Players by Least set Losses*/
 
-SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
+SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, elo, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
 FROM player
 Order By losses;
 
 /*List of Players by Least set Losses (top 10)*/
 
-SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
+SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, elo, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
 FROM player
 Order By losses
 LIMIT 10;
 
 /*List of Players by W/L Ratio, avoids divide by 0*/
 
-SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
+SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, elo, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
 FROM player
 Where losses > 0
 Order By wins/losses desc;
 
 /*List of Players by W/L Ratio, avoids divide by 0 (top 10)*/
 
-SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
+SELECT primary_nm, attended_num, wins AS Set_Wins, losses AS Set_Losses, elo, team AS Team, characters as Characters, mu, sigma, DATE_FORMAT(last_match, '%Y-%m-%d') AS Last_Match, decay_months AS Decay_Months
 FROM player
 Where losses > 0
 Order By wins/losses desc
@@ -51,7 +51,7 @@ JOIN player p2
 JOIN player p3
 	ON p3.id = s.winner;
     
-/* List of tournaments + the tournament winner */
+/* List of tournaments + the tournament winner, WHERE clause to be changed based on the input data of the parser */
 
 SELECT t.nm as Tournament_Name, date_format(t.date, '%Y-%m-%d') as Date, t.entrants_num as Number_of_Entrants, p.primary_nm AS Winner
 FROM TOURNAMENT t
@@ -66,8 +66,27 @@ WHERE s.round = "GF2" OR s.round = "GFs" OR s.round = "GF";
 
 TRIGGER:
 - wins, losses, elo, sigma, mu, and last_match of both players need automatically be updated whenever a set is added, but that goes without saying.
+- elo should be instantiated to 1000 on creation of a new player so that calculations can be performed
 - decay_months of the involved players can be regularly updated using the last_match column and subtracting the current sysdate by their last match and returning the month of that subtraction.
 - wins in head-to-head table need simply be incremented by 1 for the winner of each set using the player_id's to locate their head-to-head tuple/entry/whatever you address a row of a table as.
 	*Make sure order of the IDs in a row does NOT matter (a key of 1,2 should be the same as 2,1 for head to head purposes)
-    *^^^This can easily be solved using the OR keyword in the WHERE clause
+    *^^^This can easily be solved using the OR keyword as well as AND in the WHERE clause
 */ 
+
+SELECT *
+FROM player;
+
+SELECT *
+FROM region;
+
+SELECT *
+FROM set_data;
+
+SELECT *
+FROM tournament;
+
+SELECT winner
+	FROM set_data
+	WHERE id IN (SELECT MAX(id)
+					FROM set_data);
+
---
+++ Sample Region Tourney & Sets.sql
@@ -0,0 +1,41 @@
+/*UPDATE player
+SET elo = 1000
+WHERE id <= 250;
+
+*/
+
+INSERT INTO region(id, nm)
+VALUES(1, "Tristate");
+
+INSERT INTO tournament(id, nm, date, entrants_num, region_id)
+VALUES(1, "The Break", sysdate(), 50, 1);
+
+INSERT INTO tournament(id, nm, date, entrants_num, region_id)
+VALUES(2, "Wii Bare Bears", sysdate(), 70, 1);
+
+INSERT INTO tournament(id, nm, date, entrants_num, region_id)
+VALUES(3, "Xeno", '2007-12-30', 90, 1);
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(1, 1, 1, 1, 2, "WFs", "The Break", 1, "Bodied");
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(2, 2, 1, 2, 3, "LFs", "The Break", 3, "PWNED");
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(3, 3, 1, 1, 3, "GF1", "The Break", 3, "Reset");
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(4, 4, 1, 1, 3, "GF2", "The Break", 1, "Stolen");
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(5, 5, 2, 1, 3, "GF", "Wii Bare Bairs", 1, "Robbed");
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(6, 6, 3, 3, 1, "GFs", "Xeno", 3, "Jank");
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(7, 7, 3, 2, 3, "LFs", "Xeno", 3, "Wow");
+
+INSERT INTO set_data(id, sk_id, tournament_id, player1, player2, round, tournament_nm, winner, description)
+VALUES(8, 8, 2, 1, 2, "LFs", "Wii Bare Bairs", 1, "lmao");
